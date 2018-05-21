7288ee72861e1312d19a03ca304d2687b2867a9b
Aleksandr Antonov <antonov.a.aleksandr@gmail.com>
2018-05-20 23:46:27 -0600

    update routes to use new templates

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -2,12 +2,12 @@
 
 // Get 'home' page
 module.exports.homelist = (req, res, next) => {
-    res.render('index', { title: 'Home' });
+    res.render('locations-list', { title: 'Home' });
 };
 
 // Get 'Location info' page
 module.exports.locationInfo = (req, res, next) => {
-    res.render('index', { title: 'Location info' });
+    res.render('location-info', { title: 'Location info' });
 };
 
 // Get 'Add review' page
