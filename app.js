0c58618742fb2f22cfae176cd136f7fcd55f69d2
Aleksandr Antonov <antonov.a.aleksandr@gmail.com>
2018-06-02 10:18:07 -0600

    move db into api

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var sassMiddleware = require('node-sass-middleware');
-var indexRouter = require('./app_server/routes/index');
+var routes = require('./app_server/routes/index');
+var routesApi = require('./app_api/routes/index');
 require('./app_server/models/db');
 
 var app = express();
@@ -27,7 +28,8 @@ app.use(sassMiddleware({
 }));
 
 app.use(express.static(path.join(__dirname, '/public')));
-app.use('/', indexRouter);
+app.use('/', routes);
+app.use('/api', routesApi);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
