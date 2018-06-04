87e9a1cc3cf2cf797d6964904f30dd4a21da2ba9
Aleksandr Antonov <antonov.a.aleksandr@gmail.com>
2018-06-04 00:48:59 -0600

    configure locationsListByDistance controller

--- app_api/controllers/locations.js
+++ app_api/controllers/locations.js
@@ -3,10 +3,68 @@ const mongoose = require('mongoose');
 const Loc = mongoose.model('Location');
 const Res = require('./shared');
 
+let theEarth = (function () {
+    let earthRadius = 6371; // km, miles is 3959
+
+    let getDistanceFromRads = function(rads) {
+      return parseFloat(rads * earthRadius);
+    };
+
+    let getRadsFromDistance = function(distance) {
+      return parseFloat(distance / earthRadius);
+    };
+
+    return {
+      getDistanceFromRads : getDistanceFromRads,
+      getRadsFromDistance : getRadsFromDistance
+    };
+})();
+
 module.exports.locationsCreate = (req, res, next) => {
 }
 
 module.exports.locationsListByDistance = (req, res, next) => {
+    //http://localhost:4000/api/locations?lng=-104.98653&lat=39.732975&dist=20
+    let lng = parseFloat(req.query.lng);
+    let lat = parseFloat(req.query.lat);
+    let dist = parseFloat(req.query.dist) * 1000;
+
+    let point = {
+        type: 'Point',
+        coordinates: [lng, lat]
+    };
+
+    Loc.aggregate([{
+        $geoNear: {
+            near: point,
+            spherical: true,
+            maxDistance: dist,
+            distanceField: 'distance',
+            // convert meters to miles
+            distanceMultiplier: 0.000621,
+            // max number of entries being returned
+            num: 20
+        },
+    }, {
+        //fields to be returned
+        $project:{
+            distance: true,
+            name: true,
+            address: true,
+            rating: true,
+            facilities: true
+        }
+    }]).then((results, stats) => {
+        if (results.length > 0) {
+            Res.sendJsonResponse(res, 200, results);
+        } else {
+            Res.sendJsonResponse(res, 404, {
+                "message" : "No locations were found near your"
+            });
+        }
+    }).catch((error) => {
+        Res.sendJsonResponse(res, 404, error);
+    });
 }
 
 module.exports.locationsReadOne = (req, res, next) => {
