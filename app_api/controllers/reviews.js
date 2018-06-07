142d36b8856c59baff3be6b7ae3b8a7632e51562
Aleksandr Antonov <antonov.a.aleksandr@gmail.com>
2018-06-06 19:03:04 -0600

    add create & save review conroller, add setAverageRating function

--- app_api/controllers/reviews.js
+++ app_api/controllers/reviews.js
@@ -4,7 +4,23 @@ const Loc = mongoose.model('Location');
 const Res = require('./shared');
 
 module.exports.reviewsCreate = (req, res, next) => {
-
+    let locationId = req.params.locationId;
+    if (locationId) {
+        Loc
+            .findById(locationId)
+            .select('reviews')
+            .exec((error, location) => {
+                if (error) {
+                    Res.sendJsonResponse(res, 400, error);
+                } else {
+                    doAddReview(req, res, location);
+                }
+            });
+    } else {
+        Res.sendJsonResponse(res, 404, {
+            "message" : "Not found, [locationId] required"
+        });
+    }
 }
 
 module.exports.reviewsReadOne = (req, res, next) => {
@@ -68,3 +84,59 @@ module.exports.reviewsUpdateOne = (req, res, next) => {
 module.exports.reviewsDeleteOne = (req, res, next) => {
 
 }
+
+let doAddReview = (req, res, location) => {
+    if (!location) {
+        Res.sendJsonResponse(res, 404, {
+            "message" : "Not found, [locationId] required"
+        });
+    } else {
+       location.reviews.push({
+           author: req.body.author,
+           rating: req.body.rating,
+           review: req.body.review
+       });
+       location.save((error, location) => {
+           if (error) {
+               Res.sendJsonResponse(res, 400, error);
+            } else {
+                updateAverageRating(location._id);
+                let thisReview = location.reviews[location.reviews.length -1];
+                Res.sendJsonResponse(res, 201, thisReview);
+           }
+       }) 
+    }
+};
+
+let updateAverageRating = (locationId) => {
+    Loc
+        .findById(locationId)
+        .select('rating reviews')
+        .exec((error, location) => {
+            if (error) {
+                Res.sendJsonResponse(res, 400, error);
+            } else {
+                doSetAverageRating(location);
+            }
+        });
+};
+
+let doSetAverageRating = (location) => {
+    let i, reviewCount, ratingAverage, ratingTotal;
+    if (location.reviews && location.reviews.length > 0) {
+        reviewCount = location.reviews.length;
+        ratingTotal = 0;
+        for (i = 0; i < reviewCount; i++) {
+            ratingTotal += location.reviews[i].rating;
+        }
+        ratingAverage = parseInt(ratingTotal / reviewCount, 10);
+        location.rating = ratingAverage;
+        location.save((error) => {
+            if (error) {
+                console.error(error);
+            } else {
+                console.log('Average rating updated to', ratingAverage);
+            }
+        });
+    }
+};
